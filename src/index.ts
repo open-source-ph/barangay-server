8dc9ef442f51f3ebcb8f654ab891b0b53c2200f5
BosEriko <bos.eriko@gmail.com>
2020-02-08 09:17:00 +0800

    remove semicolon

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const Koa = require('koa');
-const app = new Koa();
+const Koa = require('koa')
+const app = new Koa()
 
 app.use(async (ctx: any) => {
-  ctx.body = 'Hello World';
-});
+  ctx.body = 'Hello World'
+})
 
-app.listen(3000);
+app.listen(3000)
